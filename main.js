55aec8067f4b2c5ec90aa94e948e1c42e7a994f3
Your Name <you@example.com>
2020-03-03 12:57:33 -0500

    ;(

--- main.js
+++ main.js
@@ -1,6 +1,86 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function fizzBuzzSingleNumber(num){
+  let result = 0;
+for(let i = 0; i < num; i++){
+  if(num % 5 === 0 && num % 3 === 0){
+    return 'FizzBuzz'} 
+if(num % 5 !== 0 && num % 3 !== 0){
+  return num
+  }
+  else if (num % 3 === 0 && num % 5 !== 0){
+  return 'Fizz'};
+  
+if(num % 5 === 0 || num % 3 !== 0)
+return 'Buzz'
+
+}
+}
+
+
+// // function getMiddleLetters(str){
+// let result = "";
+// for(let i = 0; i < str.length; i++){
+// if(result.length % 2 === 0){
+//   return 
+// }
+// if(result.length % 2 === 1){
+//   return
+// }
+// }
+// }
+
+
+// function getNextLetter(str){
+//   let result = [];
+//   const alpha = abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ;
+//   for(let i = 0; i < alpha.length;i++){
+
+//   }
+// }
+// function getAverage(num){
+// let result = 0;
+// for(let i = 0; i < num.length; i++){}
+// let 
+//}
+
+
+// function removeTrolls(cap){
+// for(cap of caps)
+// let cap = 
+
+// }
+// function triStateAreaOnly(cities){
+// for(city of cities){
+//   cities = 
+// }
+// }
+
+
+// function cipher(str){ 
+//   let result = '';
+//   for(let i = 0; i < str.length; i++){
+
+//   }
+// return result
+// }
+
+// }
+// }
+
+// function addToMultiDigitNumbers(fakeNum, arrayOfNum){
+
+// }
+
+
+// function hyphenateNames(namesMaybe){
+// let result = '';
+// for(let i = 0; i < namesMaybe.length; i++){
+
+// }
+
+// }
 
 
 
